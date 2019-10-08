28ed255db088b57798c3a0029975a52c556fbfa6
Nazeh <ar.nazeh@gmail.com>
2019-10-08 17:46:16 +0300

    proto display

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,47 @@
-"use-strict"
+'use-strict';
 
-const board = (() => {
-  const state = [[null,null,null], [null,null,null], [null,null,null]];
-  
-  const get = () => state ;
+const Board = (() => {
+  const state = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
+
+  const get = () => state;
 
   const set = (row, column, mark) => {
     state[row][column] = mark;
   };
 
-  return { get, set };
-})()
+  return {
+    get,
+    set,
+  };
+})();
+
+const UI = (() => {
+  const renderBoard = (board) => {
+    let html = '<table>';
+    board.forEach((row) => {
+      html += '<tr>';
+
+      row.forEach((cell) => {
+        html += `<td>${cell}</td>`;
+      });
+
+      html += '</tr>';
+    });
+
+    return `${html}</table>`;
+  };
+
+  const display = (board) => {
+    document.getElementById('board').innerHTML = renderBoard(board);
+  };
+
+  return {
+    display,
+  };
+})();
+
+UI.display(Board.get());
