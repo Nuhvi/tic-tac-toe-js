20cc7d0812fb122358a220606759a5e8b1b65786
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-10-08 17:17:49 +0100

    fix import errors by appending .js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
-import Board from './lib/board';
-import UI from './lib/ui';
-import Game from './lib/game';
+import Board from './lib/board.js';
+import UI from './lib/ui.js';
+import Game from './lib/game.js';
 
 UI.display(Board.get());
+UI.display(Board.set(1,1,0));
 Game.initialize(...UI.getPlayersNames());
--- assets/scripts/lib/game.js
+++ assets/scripts/lib/game.js
@@ -1,4 +1,4 @@
-import Player from './player';
+import Player from './player.js';
 
 const Game = (() => {
   let p1 = null;
