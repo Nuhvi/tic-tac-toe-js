f09a36a014d6567208f9d948a48a5204a105e11e
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-10-08 16:05:41 +0100

    add player factory

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -53,4 +53,16 @@ const UI = (() => {
   };
 })();
 
+const Player = (_name, _mark, _score = 0) => {
+  const name = () => _name;
+  const mark = () => _mark;
+  const score = () => _score;
+  const updateScore = () => _score += 1;
+  const changeMark = () => {
+    _mark === 0 ? 1 : 0;
+  }
+
+  return { name, mark, score, updateScore, changeMark }
+}
+
 UI.display(Board.get());
